e3c1b7576ed7abd2013f47d6ff9ac80b049757e7
chandan9074 <chandan15-11611@diu.edu.bd>
2021-11-10 17:37:59 +0600

    refact: Update some logic

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
+
 from .models import UserProfile
 from .serializer import UserProfileSerializer
 
